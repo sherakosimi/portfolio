e911e717c45fd58fe3ce595a735dd10813e2dd08
Kosimi <kosimis@wit.edu>
2023-01-21 16:00:00 -0500

    Cards' layout is done, animation on hover is still in progres

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -3,9 +3,49 @@ import React from "react";
 export default function Projects() {
   return (
     <section id="projects" className="section-projects">
-      <div>
-        <h2 className="heading-secondary">Personal</h2>
-        <h2 className="heading-tertiary">Projects</h2>
+      <div className="projects">
+        <div>
+          <h2 className="heading-secondary">Personal</h2>
+          <h2 className="heading-tertiary">Projects</h2>
+        </div>
+        <div className="projects__flexbox">
+          <div className="projects__card">
+            <div className="projects__card-container"></div>
+            <h2 className="projects__card-title">LifeTrackr</h2>
+            <h2 className="projects__card-subtitle">Mobile App</h2>
+          </div>
+          <div className="projects__card">
+            <div className="projects__card-container"></div>
+            <h2 className="projects__card-title">Outdoors</h2>
+            <h2 className="projects__card-subtitle">Web App</h2>
+          </div>
+          <div className="projects__card">
+            <div className="projects__card-container"></div>
+            <h2 className="projects__card-title">ETH Wallet</h2>
+            <h2 className="projects__card-subtitle">Web App</h2>
+          </div>
+          <div className="projects__card">
+            <div className="projects__card-container"></div>
+            <h2 className="projects__card-title">Team Manager</h2>
+            <h2 className="projects__card-subtitle">Mobile App</h2>
+          </div>
+          <div className="projects__card">
+            <div className="projects__card-container"></div>
+            <h2 className="projects__card-title">Portfolio</h2>
+            <h2 className="projects__card-subtitle">Web App</h2>
+          </div>
+
+          {/* <div className="projects__card">
+            <div className="projects__content">
+              <h2 className="projects__title">LifeTrackr</h2>
+              <p className="projects__copy">
+                Check out all of these gorgeous mountain trips with beautiful
+                views of, you guessed it, the mountains{" "}
+              </p>
+              <button className="projects__btn"> Learn more</button>
+            </div>
+          </div> */}
+        </div>
       </div>
     </section>
   );
