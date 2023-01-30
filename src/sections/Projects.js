c0ec9cbe62647ff3daf11f8a7674b0973492d819
Kosimi <kosimis@wit.edu>
2023-01-30 13:31:35 -0500

    Projects section design is almost done, experience section layout is done (not responsive yet)

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -9,32 +9,236 @@ export default function Projects() {
           <h2 className="heading-tertiary">Projects</h2>
         </div>
         <div className="projects__flexbox">
-          <div className="projects__card">
-            <div className="projects__card-container"></div>
-            <h2 className="projects__card-title">LifeTrackr</h2>
-            <h2 className="projects__card-subtitle">Mobile App</h2>
+          <div className="card">
+            <div className="card__top" id="web">
+              <h3 className="card__description">Full Stack & UI/UX Case</h3>
+              <ul className="card__list">
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-react" id="react"></i>
+                  </span>
+                  React Native
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-redux" id="redux"></i>
+                  </span>
+                  Redux-Saga - AJAX
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-aws" id="aws"></i>
+                  </span>
+                  AWS
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-figma" id="figma"></i>
+                  </span>
+                  Figma
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-react"></i>
+                  </span>
+                  REST API
+                </li>
+              </ul>
+            </div>
+            <div className="card__bottom">
+              <div className="card__title">
+                <h2 className="card__title--name">LifeTrackr</h2>
+                <h2 className="card__title--platform">Web App</h2>
+              </div>
+              <div className="card__more">
+                <button className="card__more--btn"></button>
+              </div>
+            </div>
           </div>
-          <div className="projects__card">
-            <div className="projects__card-container"></div>
-            <h2 className="projects__card-title">Outdoors</h2>
-            <h2 className="projects__card-subtitle">Web App</h2>
+          <div className="card">
+            <div className="card__top" id="mobile">
+              <h3 className="card__description">Front-End & UI/UX Case</h3>
+              <ul className="card__list">
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-react" id="react"></i>
+                  </span>
+                  React JS
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-html5" id="html5"></i>
+                  </span>
+                  HTML
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-sass" id="sass"></i>
+                  </span>
+                  SASS
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-figma" id="figma"></i>
+                  </span>
+                  Figma
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-react"></i>
+                  </span>
+                  REST API
+                </li>
+              </ul>
+            </div>
+            <div className="card__bottom">
+              <div className="card__title">
+                <h2 className="card__title--name">Outdoors</h2>
+                <h2 className="card__title--platform">Web App</h2>
+              </div>
+              <div className="card__more">
+                <button className="card__more--btn"></button>
+              </div>
+            </div>
           </div>
-          <div className="projects__card">
-            <div className="projects__card-container"></div>
-            <h2 className="projects__card-title">ETH Wallet</h2>
-            <h2 className="projects__card-subtitle">Web App</h2>
+          <div className="card">
+            <div className="card__top" id="web">
+              <h3 className="card__description">Front-End & UI/UX Case</h3>
+              <ul className="card__list">
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-react" id="react"></i>
+                  </span>
+                  React JS
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-html5" id="redux"></i>
+                  </span>
+                  HTML
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-sass" id="sass"></i>
+                  </span>
+                  SASS
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-figma" id="figma"></i>
+                  </span>
+                  Figma
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-react"></i>
+                  </span>
+                  REST API
+                </li>
+              </ul>
+            </div>
+            <div className="card__bottom">
+              <div className="card__title">
+                <h2 className="card__title--name">ETH Wallet</h2>
+                <h2 className="card__title--platform">Web App</h2>
+              </div>
+              <div className="card__more">
+                <button className="card__more--btn"></button>
+              </div>
+            </div>
           </div>
-          <div className="projects__card">
-            <div className="projects__card-container"></div>
-            <h2 className="projects__card-title">Team Manager</h2>
-            <h2 className="projects__card-subtitle">Mobile App</h2>
+          <div className="card">
+            <div className="card__top" id="mobile">
+              <h3 className="card__description">Full Stack & UI/UX Case</h3>
+              <ul className="card__list">
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-react" id="react"></i>
+                  </span>
+                  React Native
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-redux" id="redux"></i>
+                  </span>
+                  Redux Thunk
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-firebase" id="firebase"></i>
+                  </span>
+                  Firebase
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-figma" id="figma"></i>
+                  </span>
+                  Figma
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-react"></i>
+                  </span>
+                  Expo
+                </li>
+              </ul>
+            </div>
+            <div className="card__bottom">
+              <div className="card__title">
+                <h2 className="card__title--name">Workflow</h2>
+                <h2 className="card__title--platform">Mobile App</h2>
+              </div>
+              <div className="card__more">
+                <button className="card__more--btn"></button>
+              </div>
+            </div>
           </div>
-          <div className="projects__card">
-            <div className="projects__card-container"></div>
-            <h2 className="projects__card-title">Portfolio</h2>
-            <h2 className="projects__card-subtitle">Web App</h2>
+          <div className="card">
+            <div className="card__top" id="web">
+              <h3 className="card__description">Front-End & UI/UX Case</h3>
+              <ul className="card__list">
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-react" id="react"></i>
+                  </span>
+                  React Native
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-redux" id="redux"></i>
+                  </span>
+                  Redux-Saga - AJAX
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-aws" id="aws"></i>
+                  </span>
+                  AWS
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-figma" id="figma"></i>
+                  </span>
+                  Figma
+                </li>
+                <li className="card__list--item">
+                  <span className="card__list--icon">
+                    <i class="bx bxl-react"></i>
+                  </span>
+                  REST API
+                </li>
+              </ul>
+            </div>
+            <div className="card__bottom">
+              <div className="card__title">
+                <h2 className="card__title--name">Portfolio</h2>
+                <h2 className="card__title--platform">Web App</h2>
+              </div>
+              <div className="card__more">
+                <button className="card__more--btn"></button>
+              </div>
+            </div>
           </div>
-
           {/* <div className="projects__card">
             <div className="projects__content">
               <h2 className="projects__title">LifeTrackr</h2>
